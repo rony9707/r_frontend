5416285bb707d2ec06eabc442993288bf526c97f
Rony <50582705+rony9707@users.noreply.github.com>
2023-07-30 02:09:56 +0530

    Light Mode and Dark Mode button beautify

--- src/app/shared/dashboard/dashboard.component.ts
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -30,11 +30,16 @@ export class DashboardComponent implements OnInit {
     if (this.browserRefresh = browserRefresh) {
       //console.log('refreshed?:', browserRefresh);
       let GetTheme = JSON.parse(localStorage.getItem("PageTheme") || '{}');
+      const checkbox: any = document.getElementById('darkmode-toggle');
+      console.log(checkbox)
       if (GetTheme === "Light Mode") {
         document.body.classList.add('light-theme');
+        document.getElementById('darkmode-toggle')
+        checkbox.checked = false
       }
       else if (GetTheme === "Dark Mode") {
         document.body.classList.add();
+        checkbox.checked = true
       }
       else {
         document.body.classList.add('dark-theme');//If someone deletes data from local stroage fix
@@ -47,29 +52,46 @@ export class DashboardComponent implements OnInit {
     window.history.back();
   }
 
-  //theme
+  //theme value taken from local session
   theme: any = JSON.parse(localStorage.getItem("PageTheme") || '{}');
 
+  //checkedOrNot value taken from local session
+  checkedOrNot: any = JSON.parse(localStorage.getItem("checkedBox") || '{}');
 
 
   toggleDarkTheme(): void {
     var setTheme = document.body;
     setTheme.classList.toggle('light-theme');
     var theme;
+    var checkedBox;
+    const checkbox: any = document.getElementById('darkmode-toggle');
     if (setTheme.classList.contains('light-theme')) {
       theme = "Light Mode";
       this.theme = "Light Mode"
+      checkedBox = false
+      checkbox.check = checkedBox
+      console.log(checkbox.checked)
     }
     else {
       theme = "Dark Mode";
       this.theme = "Dark Mode"
+      checkedBox = true
+      checkbox.check = checkedBox
+      console.log(checkbox.checked)
     }
+
     localStorage.setItem("PageTheme", JSON.stringify(theme));
+    localStorage.setItem("checkedBox", JSON.stringify(checkedBox));
+
+    //* *NOTES
+    // Light= false
+    // Dark= True
+    
   }
 
-
-
 }
+
+
 
 
 
